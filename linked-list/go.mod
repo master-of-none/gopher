b8b6ddea0395688e1d4a4676c9fd92b2d995f8d9
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-01-01 11:10:18 -0500

    Singly linked list refactored ✅

--- linked-list/go.mod
+++
@@ -1,3 +0,0 @@
-module linked-list
-
-go 1.21.5
--- linkedlist/singlylist/singlyList.go
+++ linkedlist/singlylist/singlyList.go
@@ -83,21 +83,3 @@ func (list *LinkedList) traverseList() {
 	}
 	fmt.Println("-|")
 }
-
-func main() {
-	list := LinkedList{}
-
-	list.traverseList()
-	list.insertAtHead(30)
-	list.insertAtHead(20)
-	list.insertAtHead(10)
-	list.traverseList()
-	list.insertAtTail(40)
-	list.traverseList()
-	temp1 := list.deleteAtHead()
-	fmt.Printf("Deleted value at head is: %d\n", temp1)
-	list.traverseList()
-	temp2 := list.deleteAtTail()
-	fmt.Printf("Deleted value at tail is: %d\n", temp2)
-	list.traverseList()
-}
