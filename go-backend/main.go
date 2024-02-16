fdd025170b59c3f63784532af3e9635a44444f34
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-02-15 23:48:16 -0800

    Updated port code

--- go-backend/main.go
+++ go-backend/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("Port is not found configured correctly")
+	}
+
+	fmt.Println("Port: ", portString)
 }
