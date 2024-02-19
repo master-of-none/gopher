c9deb8b5af2d681f4b04f842c3a88c2e54ee764c
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-02-18 18:42:11 -0800

    Added ability to get feeds, connect to an XML feed

--- go-backend/handler_feed.go
+++ go-backend/handler_feed.go
@@ -10,9 +10,10 @@ import (
 	"github.com/master-of-none/gopher/go-backend/internal/database"
 )
 
-func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request, user database.User) {
 	type parameters struct {
 		Name string `json:"name"`
+		URL  string `json:"url"`
 	}
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
@@ -23,20 +24,18 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+	feed, err := apiCfg.DB.CeateFeed(r.Context(), database.CeateFeedParams{
 		ID:        uuid.New(),
 		CreatedAt: time.Now().UTC(),
 		UpdatedAt: time.Now().UTC(),
 		Name:      params.Name,
+		Url:       params.URL,
+		UserID:    user.ID,
 	})
 
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Cannot create user: %v", err))
+		respondWithError(w, 400, fmt.Sprintf("Cannot create feed: %v", err))
 		return
 	}
-	respondWithJSON(w, 201, databaseUserToUser(user))
-}
-
-func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
-	respondWithJSON(w, 200, databaseUserToUser(user))
+	respondWithJSON(w, 201, databaseFeedToFeed(feed))
 }
