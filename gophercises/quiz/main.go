f0b3eca9d4c4588b8198146e5139facfd01f7907
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-02-20 22:38:18 -0800

    Updated file read code

--- gophercises/quiz/main.go
+++ gophercises/quiz/main.go
@@ -1,11 +1,21 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	csvFileName := flag.String("csv", "problems.csv", "must be csv file")
+	csvFileName := flag.String("csv", "problem.csv", "must be csv file")
 
 	flag.Parse()
-	_ = csvFileName
 
+	file, err := os.Open(*csvFileName)
+
+	if err != nil {
+		fmt.Printf("Failed to open this file. Error is %v\n", err)
+		os.Exit(1)
+	}
+	_ = file
 }
