911eec08ddbf8462919515504d3da4530cd17cd0
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-02-22 20:22:39 -0800

    Added leet solutions

---
+++ leetcode/arrays/containsDuplicate.go
@@ -0,0 +1,13 @@
+package arrays
+
+func containsDuplicate(nums []int) bool {
+	hashset := make(map[int]bool)
+
+	for _, n := range nums {
+		if _, ok := hashset[n]; ok {
+			return true
+		}
+		hashset[n] = true
+	}
+	return false
+}
---
+++ leetcode/go.mod
@@ -0,0 +1,3 @@
+module github.com/master-of-none/gopher/leetcode/arrays
+
+go 1.22.0
