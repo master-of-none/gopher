068f45b253aa981020e9752312f26b802036a25c
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-02-23 22:05:41 -0800

    Updated code

--- gophercises/quiz/main.go
+++ gophercises/quiz/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"os"
@@ -14,8 +15,39 @@ func main() {
 	file, err := os.Open(*csvFileName)
 
 	if err != nil {
-		fmt.Printf("Failed to open this file. Error is %v\n", err)
+		exit(fmt.Sprintf("Failed to open this file. Error is %v\n", err))
 		os.Exit(1)
 	}
-	_ = file
+
+	reader := csv.NewReader(file)
+	lines, err := reader.ReadAll()
+
+	if err != nil {
+		exit(fmt.Sprintf("Failed to read CSV file %v\n", err))
+	}
+
+	problems := parseLines(lines)
+	fmt.Println(problems)
+}
+
+type problem struct {
+	question string
+	answer   string
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+
+	for i, line := range lines {
+		ret[i] = problem{
+			question: line[0],
+			answer:   line[1],
+		}
+	}
+
+	return ret
+}
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
