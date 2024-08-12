073d5bb0204a78b7aaeb9fef37c37d3bb1c8e0f1
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-08-11 19:29:04 -0700

    added main

---
+++ backend/chit_chat/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+	// mux.HandleFunc("/", index)
+
+	server := &http.Server{
+		Addr:    "http://localhost:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
