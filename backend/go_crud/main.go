168d8b820e3ba809bc095d9343d4be70b99c3d8a
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-08-15 16:03:07 -0400

    updated initial setup code

--- backend/go_crud/main.go
+++ backend/go_crud/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/master-of-none/go_crud/initializers"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
 
 func main() {
-	fmt.Println("Hello testing Compile Daemon")
+	r := gin.Default()
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
