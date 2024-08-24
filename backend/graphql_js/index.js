26de7125cf6efd336fc1fbf4acdba3d59b09e0e9
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-08-24 15:38:04 -0400

    Added a single review using ID
    Added singular for index and schema
    
    Related Data
    
    Mutation
    
    Mutations Done

--- backend/graphql_js/index.js
+++ backend/graphql_js/index.js
@@ -16,6 +16,47 @@ const resolvers = {
         authors() {
             return db.authors;
         },
+        review(_, args) {
+            return db.reviews.find((review) => review.id === args.id);
+        },
+        game(_, args) {
+            return db.games.find((game) => game.id == args.id);
+        },
+        author(_, args) {
+            return db.authors.find((author) => author.id === args.id);
+        },
+    },
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter((r) => r.game_id === parent.id);
+        },
+    },
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter((r) => r.author_id === parent.id);
+        },
+    },
+    Review: {
+        author(parent) {
+            return db.authors.find((author) => author.id == parent.author_id);
+        },
+        game(parent) {
+            return db.games.find((game) => game.id === parent.game_id);
+        },
+    },
+    Mutation: {
+        deleteGame(_, args) {
+            db.games = db.games.filter((g) => g.id != args.id);
+            return db.games;
+        },
+        addGame(_, args) {
+            let game = {
+                ...args.game,
+                id: Math.floor(Math.random() * 10000).toString(),
+            };
+            db.games.push(game);
+            return game;
+        },
     },
 };
 const server = new ApolloServer({
