dd1bda23208169c3ea827ba49aa471b8a185a2e6
master-of-none <shrikrishna.bht@iusevimbtw.com>
2026-05-05 12:06:36 +0530

    Added CORS

---
+++ system-design/url-shortner/mw/cors.go
@@ -0,0 +1,16 @@
+package mw
+
+import "net/http"
+
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+
+		if r.Method == http.MethodOptions {
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
